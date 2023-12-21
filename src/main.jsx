508f33b8f704b6c72e4853fc7fe7f56f2e9890f3
tiagomatos8 <tcm.matos@gmail.com>
2023-12-22 10:46:47 +1100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import ErrorPage from "./error-page";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+        loader: contactLoader
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
