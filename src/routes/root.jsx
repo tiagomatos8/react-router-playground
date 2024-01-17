31a64788e1f202a7a23206be96dc6798d54f9762
tiagomatos8 <tcm.matos@gmail.com>
2024-01-17 22:09:34 +1100

    Adding search feature

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,10 @@
 import { Form, NavLink, Outlet, redirect, useLoaderData, useNavigation } from "react-router-dom";
 import { createContact, getContacts } from '../contacts';
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
@@ -19,7 +21,7 @@ export default function Root() {
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
@@ -36,7 +38,7 @@ export default function Root() {
               className="sr-only"
               aria-live="polite"
             ></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
