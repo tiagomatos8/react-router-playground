6b991a13c21ddaa61982c5143dc5e01441c504b4
tiagomatos8 <tcm.matos@gmail.com>
2024-01-22 19:32:19 +1100

    replacing the current entry in the browser history stack

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -40,7 +40,8 @@ export default function Root() {
               name="q"
               defaultValue={q}
               onChange={(event) => {
-                submit(event.currentTarget.form);
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, { replace: !isFirstSearch });
               }}
             />
             <div
