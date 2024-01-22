281039e408371445a1156427f3899353258c8ee3
tiagomatos8 <tcm.matos@gmail.com>
2024-01-22 21:59:07 +1100

    Refactoring routes to use JSX instead of json

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoutesFromElements, createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import Contact, { loader as contactLoader, action as contactAction } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
@@ -9,41 +9,36 @@ import ErrorPage from "./error-page";
 import "./index.css";
 import Index from "./routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true, element: <Index />
-          },
-          {
-            path: 'contacts/:contactId',
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction
-          },
-          {
-            path: 'contacts/:contactId/edit',
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction
-          },
-          {
-            path: 'contacts/:contactId/destroy',
-            action: destroyAction
-          }
-        ]
-      }
-    ]
-  }
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route
+    path="/"
+    element={<Root />}
+    errorElement={<ErrorPage />}
+    loader={rootLoader}
+    action={rootAction}
+  >
+    <Route errorElement={<ErrorPage />}>
+      <Route index={true} element={<Index />} />
+      <Route 
+        path={'contacts/:contactId'}
+        element={<Contact />}
+        loader={contactLoader}
+        action={contactAction}
+      />
+      <Route
+        path={'contacts/:contactId/edit'}
+        element={<EditContact />}
+        loader={contactLoader}
+        action={editAction}
+      />
+      <Route
+        path={'contacts/:contactId/destroy'}
+        action={destroyAction}
+      />
+    </Route>
+  </Route>
+));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
